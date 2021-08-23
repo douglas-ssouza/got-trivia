2505574f9e1087c3a09b2f39391ae27d9df51271
Douglas Souza <douglassouzabjj@hotmail.com>
2021-08-23 16:50:52 -0300

    Adiciona funçao para mostrar personagem na tela

--- script.js
+++ script.js
@@ -1,31 +1,35 @@
-const fetch = require('node-fetch');
-const { start } = require('repl');
-
 const API_URL = 'https://thronesapi.com/api/v2/Characters';
 
 const main = document.querySelector('.main-section');
 const btnIniciar = document.querySelector('.btn-iniciar');
 
-const createImageSection = (element, className) => {
+const fetchCharacters = async () => {
+  const response = await fetch(API_URL);
+  const results = await response.json();
+  return results;
+};
+
+const createQuizElements = (element, className) => {
   const newElement = document.createElement(element);
   newElement.className = className;
   return newElement;
+};
+
+const addInfo = async () => {
+  const results = await fetchCharacters();
+  console.log(results);
+  const img = document.querySelector('.character-image');
+  img.src = results[0].imageUrl;
+  const btn = document.querySelector('.character-name');
+  btn.innerText = results[0].fullName;
 }
 
-const startGame = () => {
+const startGame = async () => {
   main.innerHTML = '';
-  main.appendChild(createElement('img', 'character-image'));
-  main.appendChild(createElement('button', 'character-name'));
+  main.appendChild(createQuizElements('img', 'character-image'));
+  main.appendChild(createQuizElements('button', 'character-name'));
+  await addInfo();
 };
 btnIniciar.addEventListener('click', startGame);
 
-// const fetchCharacters = async () => {
-//   const response = await fetch(API_URL);
-//   const results = await response.json();
-//   return results;
-// };
-
-// const addInfo = async () => {
-//   const characters = await fetchCharacters();
-//   document.querySelector()
-// };
+  
