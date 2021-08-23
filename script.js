4d53cb4345ffd99fdcd03b87e82251ed61f6ec4a
Douglas Souza <douglassouzabjj@hotmail.com>
2021-08-23 16:37:56 -0300

    Adiciona funçao do botao iniciar

--- script.js
+++ script.js
@@ -1,17 +1,31 @@
 const fetch = require('node-fetch');
+const { start } = require('repl');
 
 const API_URL = 'https://thronesapi.com/api/v2/Characters';
 
+const main = document.querySelector('.main-section');
 const btnIniciar = document.querySelector('.btn-iniciar');
 
-const startGame = () => {
-  const main = document.querySelector('');
+const createImageSection = (element, className) => {
+  const newElement = document.createElement(element);
+  newElement.className = className;
+  return newElement;
 }
 
-const fetchCharacters = async () => {
-  const response = await fetch(API_URL);
-  const results = await response.json();
-  console.log(results);
+const startGame = () => {
+  main.innerHTML = '';
+  main.appendChild(createElement('img', 'character-image'));
+  main.appendChild(createElement('button', 'character-name'));
 };
+btnIniciar.addEventListener('click', startGame);
 
-fetchCharacters();
+// const fetchCharacters = async () => {
+//   const response = await fetch(API_URL);
+//   const results = await response.json();
+//   return results;
+// };
+
+// const addInfo = async () => {
+//   const characters = await fetchCharacters();
+//   document.querySelector()
+// };
